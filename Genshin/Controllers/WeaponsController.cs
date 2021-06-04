221692f19d102067944b2e3ae933f9b1db80f88d
Charlie Traylor <ctraylor@tql.com>
2021-06-04 15:30:08 -0400

    Add project files.

---
+++ Genshin/Controllers/WeaponsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Genshin.Models;
+
+namespace Genshin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeaponsController : ControllerBase
+    {
+        private readonly GenshinDbContext _context;
+
+        public WeaponsController(GenshinDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Weapons
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Weapon>>> GetWeapon()
+        {
+            return await _context.Weapons.ToListAsync();
+        }
+
+        // GET: api/Weapons/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Weapon>> GetWeapon(int id)
+        {
+            var weapon = await _context.Weapons.FindAsync(id);
+
+            if (weapon == null)
+            {
+                return NotFound();
+            }
+
+            return weapon;
+        }
+
+        // PUT: api/Weapons/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWeapon(int id, Weapon weapon)
+        {
+            if (id != weapon.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(weapon).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeaponExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Weapons
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Weapon>> PostWeapon(Weapon weapon)
+        {
+            _context.Weapons.Add(weapon);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWeapon", new { id = weapon.Id }, weapon);
+        }
+
+        // DELETE: api/Weapons/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Weapon>> DeleteWeapon(int id)
+        {
+            var weapon = await _context.Weapons.FindAsync(id);
+            if (weapon == null)
+            {
+                return NotFound();
+            }
+
+            _context.Weapons.Remove(weapon);
+            await _context.SaveChangesAsync();
+
+            return weapon;
+        }
+
+        private bool WeaponExists(int id)
+        {
+            return _context.Weapons.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Genshin/Data/GenshinDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Genshin.Models;
+
+    public class GenshinDbContext : DbContext
+    {
+        public GenshinDbContext (DbContextOptions<GenshinDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Genshin.Models.Weapon> Weapons { get; set; }
+    }
